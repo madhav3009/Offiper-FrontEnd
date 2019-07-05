84cb4212b4e193c25df8b133b34977a01bf101e9
sarru1291 <sarru1291@gmail.com>
2019-07-05 15:17:39 +0530

    navbar 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.scss';
+import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div class="container">
-    <h1>Offipers</h1>
-    <h2>run your workspace smoothly</h2>
-    <h3>hkjk</h3>
-     </div>
+      <Navbar/> 
+    </div>
   );
 }
 
