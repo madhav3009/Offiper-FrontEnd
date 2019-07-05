a84523651900f87f78f9818c6f10227b0fc20c31
sarru1291 <sarru1291@gmail.com>
2019-07-05 17:51:02 +0530

    added wrapper & header update

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,8 +3,8 @@ import "./navbar.scss";
 
 const Navbar = props => {
   return (
-    <div class="container">
-      <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+    <header>
+      <nav class="navbar navbar-expand-xl sticky-top navbar-dark bg-dark">
         <a class="navbar-brand" href="#">
           Offiper
         </a>
@@ -19,61 +19,37 @@ const Navbar = props => {
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
-            <li class="nav-item active pad">
+            <li class="nav-item active">
               <a class="nav-link" href="#">
                 Stationary
               </a>
             </li>
-            <li class="nav-item active pad">
+            <li class="nav-item active">
               <a class="nav-link" href="#">
                 Hardware
               </a>
             </li>
-            <li class="nav-item active pad">
+            <li class="nav-item active">
               <a class="nav-link" href="#">
                 Decor
               </a>
             </li>
-            <li class="nav-item active pad">
+            <li class="nav-item active">
               <a class="nav-link" href="#">
                 Events
               </a>
             </li>
-            <li class="nav-item active pad">
+            <li class="nav-item active">
               <a class="nav-link" href="#">
                 Cleansing
               </a>
             </li>
-            <li class="nav-item active pad">
+            <li class="nav-item active">
               <a class="nav-link" href="#">
                 Kitchen
               </a>
             </li>
-            <li class="nav-item dropdown pad">
-              <a
-                class="nav-link dropdown-toggle active"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                More
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">
-                  Action
-                </a>
-                <a class="dropdown-item" href="#">
-                  Another action
-                </a>
-                <div class="dropdown-divider" />
-                <a class="dropdown-item" href="#">
-                  Something else here
-                </a>
-              </div>
-            </li>
+            
           </ul>
           <div class="d-flex" id="navbar-icon">
             <i class="fa fa-search" />
@@ -81,12 +57,12 @@ const Navbar = props => {
             <i class="fa fa-shopping-cart" />
           </div>
           <div className="d-flex" id="navbar-sign-button">
-            <div >signin</div>
-            <div >signup</div>
+            <div >SignIn</div>&nbsp;
+            <div >SignUp</div>
           </div>
         </div>
       </nav>
-    </div>
+    </header>
   );
 };
 
