34b36b6691c7707e8ab94d503473cd45ddd668be
sarru1291 <sarru1291@gmail.com>
2019-07-08 11:25:51 +0530

    footer 1.0.1

---
+++ src/components/itembanner/itemBanner.js
@@ -0,0 +1,142 @@
+import React, { Component } from "react";
+import "./itemBanner.scss";
+
+class Itembanner extends Component {
+  render() {
+    return (
+      <div id="FirstContainer">
+        <div className="TitleContainer">
+          <div className="bar TitleHeading" />
+          <div className="TitleHeading">
+            <h1 className="itemHeading">STATIONARY</h1>{" "}
+          </div>
+          <div className="bar TitleHeading" />
+        </div>
+
+        <div
+          id="SecondContainer"
+          style={{ display: "flex", overflowX: "auto", width: "100%" }}
+        >
+          <div className="Items">
+            <img
+              src="https://images.unsplash.com/photo-1510936994138-07e06c7c5add?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=755&q=80"
+              alt="Chania"
+            />
+            <div className="item-details">
+              <h5 className="productTitle">Pens</h5>
+              <h5 className="productTitle" id="price">
+                Rs 500
+              </h5>
+            </div>
+          </div>
+
+          <div className="Items">
+            <img
+              src="https://images.unsplash.com/photo-1510936994138-07e06c7c5add?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=755&q=80"
+              alt="Chania"
+            />
+            <div className="item-details">
+              <h5 className="productTitle">Pens</h5>
+              <h5 className="productTitle" id="price">
+                Rs 500
+              </h5>
+            </div>
+          </div>
+
+          <div className="Items">
+            <img
+              src="https://images.unsplash.com/photo-1510936994138-07e06c7c5add?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=755&q=80"
+              alt="Chania"
+            />
+            <div className="item-details">
+              <h5 className="productTitle">Pens</h5>
+              <h5 className="productTitle" id="price">
+                Rs 500
+              </h5>
+            </div>
+          </div>
+
+          <div className="Items">
+            <img
+              src="https://images.unsplash.com/photo-1510936994138-07e06c7c5add?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=755&q=80"
+              alt="Chania"
+            />
+            <div className="item-details">
+              <h5 className="productTitle">Pens</h5>
+              <h5 className="productTitle" id="price">
+                Rs 500
+              </h5>
+            </div>
+          </div>
+
+          <div className="Items">
+            <img
+              src="https://images.unsplash.com/photo-1510936994138-07e06c7c5add?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=755&q=80"
+              alt="Chania"
+            />
+            <div className="item-details">
+              <h5 className="productTitle">Pens</h5>
+              <h5 className="productTitle" id="price">
+                Rs 500
+              </h5>
+            </div>
+          </div>
+
+          <div className="Items">
+            <img
+              src="https://images.unsplash.com/photo-1510936994138-07e06c7c5add?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=755&q=80"
+              alt="Chania"
+            />
+            <div className="item-details">
+              <h5 className="productTitle">Pens</h5>
+              <h5 className="productTitle" id="price">
+                Rs 500
+              </h5>
+            </div>
+          </div>
+
+          <div className="Items">
+            <img
+              src="https://images.unsplash.com/photo-1510936994138-07e06c7c5add?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=755&q=80"
+              alt="Chania"
+            />
+            <div className="item-details">
+              <h5 className="productTitle">Pens</h5>
+              <h5 className="productTitle" id="price">
+                Rs 500
+              </h5>
+            </div>
+          </div>
+
+          <div className="Items">
+            <img
+              src="https://images.unsplash.com/photo-1510936994138-07e06c7c5add?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=755&q=80"
+              alt="Chania"
+            />
+            <div className="item-details">
+              <h5 className="productTitle">Pens</h5>
+              <h5 className="productTitle" id="price">
+                Rs 500
+              </h5>
+            </div>
+          </div>
+
+          <div className="Items">
+            <img
+              src="https://images.unsplash.com/photo-1510936994138-07e06c7c5add?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=755&q=80"
+              alt="Chania"
+            />
+            <div className="item-details">
+              <h5 className="productTitle">Pens</h5>
+              <h5 className="productTitle" id="price">
+                Rs 500
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Itembanner;
