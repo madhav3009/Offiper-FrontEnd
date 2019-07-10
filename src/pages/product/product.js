ce62962ad44922af596112ccb0976fa501e43c9d
sarru1291 <sarru1291@gmail.com>
2019-07-10 11:10:24 +0530

    product-page

--- src/pages/product/product.js
+++ src/pages/product/product.js
@@ -1,13 +1,35 @@
 import React from "react";
 import "./product.scss";
+import Quantity from "../../components/quantity/quantity";
+import Cart from "../../components/cart/cart";
 
 const Product = props => {
-  console.log(props.location.state);
 
+  console.log(props.match.url);
+  var regex = /[A-Z]/;
+  const url = props.match.url;
+  console.log(url);
+  console.log(url.match(regex));
   return (
-    <div className="d-flex">
-     <div><img src={props.location.state.src} alt=""/></div>
-     <div>{props.location.state.title}</div>
+    <div>
+      <div class="breadcrumb-box">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="/">Home</a></li>
+          <li class="breadcrumb-item"><a href="#">Library</a></li>
+          <li class="breadcrumb-item active" aria-current="page">Data</li>
+        </ol>
+      </div>
+
+      <div className="d-flex flex-wrap pt-4 flex-row justify-content-around ">
+        <img class="image-responsive" src={props.location.state.src} alt="" />
+
+        <div>
+          {props.location.state.title}
+          <div>Rs. 500</div>
+          <Quantity />
+          <div className="btn btn-secondary">add</div>
+        </div>
+      </div>
     </div>
   );
 };
