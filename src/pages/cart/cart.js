2bda61ad5cf0f734f6c61321ee4fc49c958e5c06
Madhav <36961049+madhav3009@users.noreply.github.com>
2019-07-12 17:27:55 +0530

    shoppingcart

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { CartContextConsumer } from "../../context/cart.context";
+import './cart.scss';
 
 const Cart = props => {
 	const { products } = useContext(CartContextConsumer);
@@ -8,8 +9,83 @@ const Cart = props => {
 	});
 	return (
 		<div>
-			<ul>{pro}</ul>
-		</div>
+			<div class="cartHead">
+             <h3>Shopping Cart </h3>
+			 <button >SUBTOTAL RS 2000</button>
+			 <button >PROCEED TO PAY</button>
+			 </div>
+     
+  
+  
+  
+  <div class="d-flex bg-secondary "> 
+   
+    <div class="pl"><img src="https://images.unsplash.com/photo-1527597771870-8f1e06ff8d8c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=753&q=80" style={{width:"150px",height:"auto",paddingRight:"15%"}}></img></div>
+    
+	<div class="pl"><span>Item Heading</span><br />InStock <br /><button>Add To Bucket</button></div>
+    
+	<div class="pleft"><span>Price</span><br />500<br /><strike>1000</strike> </div>
+	<div class="plefty"><span>Quantity</span><br /><select><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option></select> </div>
+	
+	
+	</div>
+    
+
+	<div class="d-flex bg-secondary "> 
+   
+   <div class="pl"><img src="https://images.unsplash.com/photo-1527597771870-8f1e06ff8d8c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=753&q=80" style={{width:"150px",height:"auto",paddingRight:"15%"}}></img></div>
+   
+   <div class="pl"><span>Item Heading</span><br />InStock <br /><button>Add To Bucket</button></div>
+   
+   <div class="pleft"><span>Price</span><br />500<br /><strike>1000</strike> </div>
+   <div class="plefty"><span>Quantity</span><br /><select><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option></select> </div>
+   
+   
+   </div>
+   
+
+   <div class="d-flex bg-secondary "> 
+   
+   <div class="pl"><img src="https://images.unsplash.com/photo-1527597771870-8f1e06ff8d8c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=753&q=80" style={{width:"150px",height:"auto",paddingRight:"15%"}}></img></div>
+   
+   <div class="pl"><span>Item Heading</span><br />InStock <br /><button>Add To Bucket</button></div>
+   
+   <div class="pleft"><span>Price</span><br />500<br /><strike>1000</strike> </div>
+   <div class="plefty"><span>Quantity</span><br /><select><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option></select> </div>
+   
+   
+   </div>
+   
+   <div class="d-flex bg-secondary "> 
+   
+   <div class="pl"><img src="https://images.unsplash.com/photo-1527597771870-8f1e06ff8d8c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=753&q=80" style={{width:"150px",height:"auto",paddingRight:"15%"}}></img></div>
+   
+   <div class="pl"><span>Item Heading</span><br />InStock <br /><button>Add To Bucket</button></div>
+   
+   <div class="pleft"><span>Price</span><br />500<br /><strike>1000</strike> </div>
+   <div class="plefty"><span>Quantity</span><br /><select><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option></select> </div>
+   
+   
+   </div>
+   
+   <div class="d-flex bg-secondary "> 
+   
+   <div class="pl"><img src="https://images.unsplash.com/photo-1527597771870-8f1e06ff8d8c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=753&q=80" style={{width:"150px",height:"auto",paddingRight:"15%"}}></img></div>
+   
+   <div class="pl"><span>Item Heading</span><br />InStock <br /><button>Add To Bucket</button></div>
+   
+   <div class="pleft"><span>Price</span><br />500<br /><strike>1000</strike> </div>
+   <div class="plefty"><span>Quantity</span><br /><select><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option></select> </div>
+   
+   
+   </div>
+   
+
+	
+	
+	</div>
+
+
 	);
 };
 
